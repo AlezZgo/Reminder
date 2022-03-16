c2cd00a63257f9e480ccc45b0e1750ed4d0bf2a3
Alexander <alez.grante@gmail.com>
2022-03-17 00:03:04 +0300

    Material Time picker

--- app/src/main/java/com/example/reminder/MainActivity.kt
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.reminder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.reminder.databinding.ActivityMainBinding
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         binding.button.setOnClickListener {
-            //TODO set up alarm
+            val materialTimePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setMinute(0)
+                .setHour(12)
+                .setTitleText("Set the time of alarm")
+                .build().apply {
+                    addOnPositiveButtonClickListener {
+                        val calendar = Calendar.getInstance()
+                        calendar.set(Calendar.SECOND, 0)
+                        calendar.set(Calendar.MILLISECOND, 0)
+                        calendar.set(Calendar.MINUTE, this.minute)
+                        calendar.set(Calendar.HOUR_OF_DAY, this.hour)
+                    }
+                }
+            materialTimePicker.show(supportFragmentManager,"tag_picker")
         }
         setContentView(binding.root)
     }
