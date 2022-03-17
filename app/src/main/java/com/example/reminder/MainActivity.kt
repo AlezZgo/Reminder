bf129d2bbb8bded03409ca42391ac0e8d5d7c02b
Alexander <alez.grante@gmail.com>
2022-03-17 23:25:30 +0300

    clear

--- app/src/main/java/com/example/reminder/MainActivity.kt
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -5,7 +5,6 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.provider.AlarmClock
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.reminder.databinding.ActivityMainBinding
@@ -20,44 +19,49 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         binding.button.setOnClickListener {
 
-            timePicker().show(supportFragmentManager,"tag_picker")
+            timePicker().show(supportFragmentManager, "tag_picker")
         }
         setContentView(binding.root)
     }
 
     private fun timePicker() = MaterialTimePicker.Builder()
-    .setTimeFormat(TimeFormat.CLOCK_12H)
-    .setMinute(0)
-    .setHour(12)
-    .setTitleText("Set the time of alarm")
-    .build().apply {
-        addOnPositiveButtonClickListener {
-            val calendar = Calendar.getInstance()
-            calendar.set(Calendar.SECOND, 0)
-            calendar.set(Calendar.MILLISECOND, 0)
-            calendar.set(Calendar.MINUTE, this.minute)
-            calendar.set(Calendar.HOUR_OF_DAY, this.hour)
+        .setTimeFormat(TimeFormat.CLOCK_12H)
+        .setMinute(0)
+        .setHour(12)
+        .setTitleText("Set the time of alarm")
+        .build().apply {
+            addOnPositiveButtonClickListener {
+                val calendar = Calendar.getInstance()
+                calendar.set(Calendar.SECOND, 0)
+                calendar.set(Calendar.MILLISECOND, 0)
+                calendar.set(Calendar.MINUTE, this.minute)
+                calendar.set(Calendar.HOUR_OF_DAY, this.hour)
 
-            val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-            val info = AlarmManager.AlarmClockInfo(calendar.timeInMillis ,pendingIntent())
-            alarmManager.setAlarmClock(info,alarmActionPendingIntent())
-            val simpleDateFormat = SimpleDateFormat("HH:mm",Locale.getDefault())
-            Toast.makeText(applicationContext,"AlarmClock ${simpleDateFormat.format(calendar.time)}",Toast.LENGTH_LONG).show()
+                val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                val info = AlarmManager.AlarmClockInfo(calendar.timeInMillis, pendingIntent())
+                alarmManager.setAlarmClock(info, alarmActionPendingIntent())
+                val simpleDateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+                Toast.makeText(applicationContext,
+                    "AlarmClock ${simpleDateFormat.format(calendar.time)}",
+                    Toast.LENGTH_LONG).show()
+            }
         }
-    }
 
     private fun pendingIntent(): PendingIntent {
-        val alarmInfoIntent = Intent(this,MainActivity::class.java).apply {
+        val alarmInfoIntent = Intent(this, MainActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
-        return PendingIntent.getActivity(this,0,alarmInfoIntent,PendingIntent.FLAG_UPDATE_CURRENT)
+        return PendingIntent.getActivity(this,
+            0,
+            alarmInfoIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
     }
 
     private fun alarmActionPendingIntent(): PendingIntent {
-        val intent = Intent(this,AlarmActivity::class.java )
+        val intent = Intent(this, AlarmActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        return PendingIntent.getActivity(this,1,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+        return PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
     }
 }
